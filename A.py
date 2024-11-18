9fdbd81d398ceb31b3ab0e9f1dbea42cce0aef29
Carson Koukkos <28ckoukkos@wpsstudents.org>
2024-11-18 16:03:18 -0500

    jhfkjd

--- A.py
+++ A.py
@@ -2,12 +2,12 @@
 # you aren't going to know what they are doing until after they push their changes
 # if you need a refresher of what a madlib is, go here: https://www.madlibs.com/MadLibs-Teachers-Guide-nocrops.pdf
 
-year_in_the_future = 0 # assign numbers or strings to the end of these versions
-wacky_robot_name = ""
-adjective_1 = ""
-violent_verb = ""
-tech_company = ""
-dollar_amount = ""
-adjective_2 = ""
-noun_1 = ""
-large_store = ""
+year_in_the_future = 200 # assign numbers or strings to the end of these versions
+wacky_robot_name = "Dingalicious"
+adjective_1 = "STINKY"
+violent_verb = "Retch"
+tech_company = "Nvidia"
+dollar_amount = "567643674"
+adjective_2 = "spooky"
+noun_1 = "gusset"
+large_store = "ikea"
